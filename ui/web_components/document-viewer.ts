37abe7eb78b2dc1a7a6d2d453c352b745479125c
Nuclear Squid <leo@cazenave.cc>
2024-06-11 17:30:44 +0200

    WIP: pagedjs integration.

--- ui/web_components/document-viewer.ts
+++ ui/web_components/document-viewer.ts
@@ -1,6 +1,7 @@
 // access the pre-bundled global API functions
 import { invoke } from "@tauri-apps/api/tauri";
 import { listen } from "@tauri-apps/api/event";
+import { Previewer } from "pagedjs";
 
 interface Payload {
     stylesheet: string,
@@ -12,8 +13,8 @@ export default class DocumentViewer extends HTMLElement {
         super();
         const shadow = this.attachShadow({ mode: "open" });
         const go_back_button = document.createElement("button");
-        const style = document.createElement("style");
-        const page  = document.createElement("div");
+        const print_button = document.createElement("button");
+        const doc = document.createElement("div");
 
         go_back_button.innerText = "Go back to main page";
         go_back_button.onclick = () => {
@@ -22,22 +23,27 @@ export default class DocumentViewer extends HTMLElement {
                 composed: true,
             }));
         };
-        page.id = "page";
-        page.style.cssText = "margin-top: 3em;";
+        print_button.innerText = "print";
+        print_button.onclick = () => {
+            window.print()
+        };
+        doc.style.cssText = "margin-top: 3em; background-color: white; height: 100%; width: 100%";
 
         shadow.appendChild(go_back_button);
-        shadow.appendChild(style);
-        shadow.appendChild(page);
+        shadow.appendChild(print_button);
+        shadow.appendChild(doc);
 
-        invoke<Payload>('load_document', { documentPath }).then(response => {
-            style.innerHTML = response.stylesheet;
-            page .innerHTML = response.content;
-        });
+        const render_document = (new_doc: Payload) => {
+            const paged = new Previewer();
+            // paged.preview(new_doc.content, [], document.body);
+            paged.preview(new_doc.content, [], doc);
+        };
 
-        listen<{ message: Payload }>("document-modified", ({ payload: { message } }) => {
-            style.innerHTML = message.stylesheet;
-            page .innerHTML = message.content;
-        });
+        // Render document on initial load
+        invoke<Payload>('load_document', { documentPath }).then(render_document);
+
+        // Rerender document when document is modified
+        listen<{ message: Payload }>("document-modified", payload => render_document(payload.message))
     }
 }
 customElements.define("document-viewer", DocumentViewer);
